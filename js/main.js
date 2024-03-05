5be510c444028825e5e16356dbfab6db43a75bb6
Mike Ermilov <98714565+Mike6869@users.noreply.github.com>
2024-03-06 02:04:32 +0300

    6.03 конец

--- js/main.js
+++ js/main.js
@@ -1,14 +1,50 @@
 const slider = document.querySelector('.js-next');
 const slides = slider.querySelectorAll('.js-next .slide');
+var initial;
 
 const slideCount = slides.length;
 let activeSlide = 0;
 
-setInterval(() => {
+if (!($(slides).hasClass('-visible')))
+  slides[activeSlide].classList.add('-visible');
+
+  for  (let i = 0; i < slides.length; i++)
+  $(slides[i].firstElementChild).attr('style', 'width:' + $(window).width() + 'px; height:' + $(window).height() + 'px');
+
+
+function SlidePhoto()
+  {  
     slides[activeSlide].classList.remove('-visible');
     activeSlide++;
-    if (activeSlide === slideCount) {
+    if (activeSlide === slideCount) 
       activeSlide = 0;
-    }
     slides[activeSlide].classList.add('-visible');
-  }, 5000);
+  }
+
+  function SlidePhotoTimeOut()
+    {
+      initial = setInterval(() => SlidePhoto(), 5000);
+    }
+
+  SlidePhotoTimeOut()
+$(document).mouseup( function(e)
+  {
+    var header = $(".splash-header");
+    var footer = $(".cover-bottom");
+
+    if (!header.is(e.target) && header.has(e.target).length === 0 && !footer.is(e.target) && footer.has(e.target).length === 0)
+      {
+        SlidePhoto();
+        clearInterval( initial )
+        initial = setInterval(() => SlidePhoto(), 5000);
+      }
+  });
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+$( window ).resize(function()
+  {
+    for  (let i = 0; i < slides.length; i++)
+      $(slides[i].firstElementChild).attr('style', 'width:' + $(window).width() + 'px; height:' + $(window).height() + 'px');
+  });
