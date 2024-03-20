58a93f8f0323b2360c3dcdaec7ea5027454e79db
Mike Ermilov <98714565+Mike6869@users.noreply.github.com>
2024-03-21 00:56:23 +0300

    21.03 Start Header

---
+++ acollins/components/elements/HeaderSpan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderSpan = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeaderSpan;
---
+++ acollins/components/modules/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <span></span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
