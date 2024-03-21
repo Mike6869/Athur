9b3f1f96ff8f74ca1da5f07e1cfcac4f396c2e07
sqerr <sqerr.yr@yandex.ru>
2024-03-21 15:33:30 +0300

    next хрень

--- acollins/components/layouts/Layout.jsx
+++ acollins/components/layouts/Layout.jsx
@@ -1,8 +1,14 @@
+// 'use client'
+
 import Header from "../modules/Header/Header";
+// 
+
 
 const layout = (children) => {
+    console.log('123')
     return (
         <>
+            <img src="http://127.0.0.1:5000/img" width={200} />
             <Header>{children}</Header>
         </>
     );
