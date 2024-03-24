783a92b16bf00304fa862d174f1f9035b5275272
sqerr <you@example.com>
2024-03-24 22:08:57 +0300

    Тест пуш

--- api_react_python/test.py
+++ api_react_python/test.py
@@ -1,25 +1,14 @@
 from flask import Flask, jsonify, url_for, send_file
 import os
 from flask_cors import CORS
-import base64
 app = Flask(__name__)
 CORS(app)
 
 IMG_TYPE = (".jpeg", ".jpg", ".png")
 VIDEO_TYPE = ("mp4")
 
-@app.route('/entry')
-def entry_return():
-    test = {'test': 123}
-    return jsonify(test)
-
 
 CORS(app)
-
-
-# @app.get('/img')
-# def get_img():
-#     return send_file("1.jpg")
 
 
 @app.get('/slider_src')
@@ -32,11 +21,6 @@ def get_img_for_src(src: str) -> 'Response':
     if (os.path.exists(src) and (src.endswith(IMG_TYPE) or src.endswith(VIDEO_TYPE))):
         return send_file(src)
     return "File not found", 404
-
-
-# @app.get('/get_video/<test>')
-# def get_video_for_src(test):
-#     return send_file(f'video/{test}')
 
 
 if __name__ == '__main__':
